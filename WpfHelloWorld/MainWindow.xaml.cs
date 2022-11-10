abc718cf024fde04114daf3fafc102668061f242
codeblackmod <schwarz-daniel@hotmail.com>
2022-11-10 18:25:06 +0100

    Introduction to type DependencyProperty and styling a button with a trigger.

--- WpfHelloWorld/MainWindow.xaml.cs
+++ WpfHelloWorld/MainWindow.xaml.cs
@@ -20,6 +20,15 @@ namespace WpfHelloWorld
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int MyProperty
+        {
+            get { return (int)GetValue(myDependencyProperty); }
+            set { SetValue(myDependencyProperty, value); }
+        }
+
+        public static readonly DependencyProperty myDependencyProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
         public MainWindow()
         {
             InitializeComponent();
